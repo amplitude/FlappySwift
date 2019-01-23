027d43478f4ae5e50436ab4543af60311673ebbe
Daniel Jih <daniel@amplitude.com>
2019-01-23 14:38:35 -0800

    install amplitude ios sdk via cocoapods

--- FlappyBird/AppDelegate.swift
+++ FlappyBird/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Amplitude_iOS
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        Amplitude.instance()?.initializeApiKey("this is a test");
+        
         // Override point for customization after application launch.
         return true
     }
---
+++ Podfile
@@ -0,0 +1,4 @@
+target 'FlappyBird' do
+  use_frameworks!
+  pod 'Amplitude-iOS', '~> 4.5.0'
+end
